98e20f2f6d3e6b9ba029e83502f162529840ec5d
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-09-08 07:53:12 +0800

    Реализация интерфейса IDisposable в базовой модели представления

--- ViewModels/Base/BaseViewModel.cs
+++ ViewModels/Base/BaseViewModel.cs
@@ -27,7 +27,15 @@ namespace TinkoffTradeSimulator.ViewModels.Base
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
         }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+           if(!Disposing || _Disposed)  return; 
+           _Disposed = true;
+        }       
     }
 }
